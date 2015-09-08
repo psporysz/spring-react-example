9ca9840c708e3375996a4242391426fafba1af21
Przemyslaw Sporysz <p.sporysz@gmail.com>
2015-09-08 18:39:54 +0200

    log eval time of JS

--- src/main/java/com/winterbe/react/React.java
+++ src/main/java/com/winterbe/react/React.java
@@ -1,25 +1,34 @@
 package com.winterbe.react;
 
-import jdk.nashorn.api.scripting.NashornScriptEngine;
-
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
 public class React {
+	
+	private static final Logger logger = LoggerFactory.getLogger(React.class);
 
     private ThreadLocal<NashornScriptEngine> engineHolder = new ThreadLocal<NashornScriptEngine>() {
         @Override
         protected NashornScriptEngine initialValue() {
             NashornScriptEngine nashornScriptEngine = (NashornScriptEngine) new ScriptEngineManager().getEngineByName("nashorn");
             try {
+            	logger.info("Initializing nashorn engine...");
+            	long before = System.currentTimeMillis();
                 nashornScriptEngine.eval(read("static/nashorn-polyfill.js"));
                 nashornScriptEngine.eval(read("static/vendor/react.js"));
                 nashornScriptEngine.eval(read("static/vendor/showdown.min.js"));
                 nashornScriptEngine.eval(read("static/commentBox.js"));
+            	logger.info("Evaluated all files in " + (System.currentTimeMillis() - before) + "ms");
             } catch (ScriptException e) {
                 throw new RuntimeException(e);
             }
